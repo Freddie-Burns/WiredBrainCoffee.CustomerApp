48662dda5dfacc1f98670e6fbd06e86001447548
RENISHAW\fb142897 <freddie.burns@renishaw.com>
2019-04-08 10:33:45 +0100

    Json database
    A lot code added to implement a json database for saving and loading customers.

--- WiredBrainCoffee.CustomerApp/MainPage.xaml.cs
+++ WiredBrainCoffee.CustomerApp/MainPage.xaml.cs
@@ -2,14 +2,42 @@
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using System;
+using WiredBrainCoffee.CustomerApp.DataProvider;
+using Windows.ApplicationModel;
+using System.Linq;
+using WiredBrainCoffee.CustomerApp.Model;
 
 namespace WiredBrainCoffee.CustomerApp
 {
     public sealed partial class MainPage : Page
     {
+        private CustomerDataProvider _customerDataProvider;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            customerListView.Items.Clear();
+
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach (var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+
+        private async void App_Suspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomerAsync(
+                customerListView.Items.OfType<Customer>());
+            deferral.Complete();
         }
 
         private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
