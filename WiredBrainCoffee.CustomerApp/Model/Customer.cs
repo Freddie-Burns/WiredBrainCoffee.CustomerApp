8c3d58c63d71f13864ad8071701e6ca4d30c62f5
RENISHAW\fb142897 <freddie.burns@renishaw.com>
2019-04-08 11:48:40 +0100

    Built release version and added OnPropertyChanged method in new Observable class.

--- WiredBrainCoffee.CustomerApp/Model/Customer.cs
+++ WiredBrainCoffee.CustomerApp/Model/Customer.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WiredBrainCoffee.CustomerApp.Base;
 
 namespace WiredBrainCoffee.CustomerApp.Model
 {
-    class Customer
+    class Customer : Observable
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public bool IsDeveloper { get; set; }
+        private string _firstName;
+        private string _lastName;
+        private bool _isDeveloper;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsDeveloper
+        {
+            get => _isDeveloper;
+            set
+            {
+                _isDeveloper = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
